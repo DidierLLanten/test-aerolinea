e31087ab8a6ad5d30a5c85034eaf942f2d258ff8
DidierLLantenPc <didiervelez@hotmail.es>
2024-09-16 04:39:36 -0500

    Eliminado ejemplo WeatherForecast

--- WebApiAerolinea/obj/Debug/net7.0/WebApiAerolinea.AssemblyInfo.cs
+++ WebApiAerolinea/obj/Debug/net7.0/WebApiAerolinea.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiAerolinea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+171d17cb6bd50efa2869746581b7aa1de39e1890")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84e14ae8486a8d417183e752f3f718f57a8653cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiAerolinea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiAerolinea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
